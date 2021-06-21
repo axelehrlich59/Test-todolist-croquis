05343ab4fee449bfe43d00027146f43d86eae9d9
Axel <axelehrlich8@gmail.com>
2021-06-21 16:28:20 +0200

    try to fix bug changes cross items delete one

--- todo.js
+++ todo.js
@@ -1,4 +1,35 @@
 
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+} 
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+
 function valeur() {
 var inputValue = document.getElementById("myOutput").value;
  var li = document.createElement("li");
@@ -10,5 +41,19 @@ var inputValue = document.getElementById("myOutput").value;
      alert("Vous devez remplir le champ");
  } else {
      document.getElementById("myUL").appendChild(li);
- }
+ } document.getElementById("myOutput").value = "";
+
+ var span = document.createElement("SPAN");
+ var txt = document.createElement("\u00D7");
+ span.className = "close";
+ span.appendChild(txt);
+ li.appendChild(span);
+
+
+ for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }}
+ 
 };
