4e4620b3c4ffa91335252c20a5005a23b9a9e035
Axel <axelehrlich8@gmail.com>
2021-06-23 17:13:22 +0200

    try to config sass file

--- todo.js
+++ todo.js
@@ -1,1 +1,42 @@
- 
+ const form = document.querySelector("form");
+ const list = document.getElementById("list");
+ const item = document.getElementById("item");
+
+
+// Add element 
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  list.innerHTML +=`<li>${item.value}</li>`;
+  storage();
+  item.value = "";
+});
+
+// Remove element
+
+list.addEventListener('click', (e) => {
+  if(e.target.classList.contains('checked')){
+    e.target.remove();
+  } else {
+    e.target.classList.add('checked');
+  }
+  storage();
+})
+
+// Storage 
+
+function storage() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getValues() {
+  let storageContent = window.localStorage.todoList;
+  if(!storageContent) {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`
+  }
+  else {
+    list.innerHTML = storageContent;
+  }
+}
+
+getValues();
